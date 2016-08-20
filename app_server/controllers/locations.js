d59fddbd36021efb499458d572bfd38a6904cbdd
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-08-21 00:43:54 +0200

    2016-08-21 moving data to controllers, before includes and mixins

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,30 @@
 module.exports.homeList = function (req, res) {
-    res.render('locations-list', { title: 'Home' });
+    res.render('locations-list', {
+        title: 'locator8 - find a place to work with wifi',
+        pageHeader: {
+            title: "locator8",
+            strapline: "Find places to work with wifi near you!"
+        },
+        locations: [{
+            name: 'Starcups',
+            adress: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '100m'
+        }, {
+            name: 'Cafe Hero',
+            adress: '125 High Street, Reading, RG6 1PS',
+            rating: 4,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '200m'
+        }, {
+            name: 'Burger Queen',
+            adress: '125 High Street, Reading, RG6 1PS',
+            rating: 2,
+            facilities: ['Food', 'Premium wifi'],
+            distance: '250m'
+        }]
+    });
 };
 
 module.exports.locationInfo = function (req, res) {
@@ -7,5 +32,5 @@ module.exports.locationInfo = function (req, res) {
 };
 
 module.exports.addReview = function (req, res) {
-    res.render('index', { title: 'Add review' });
+    res.render('location-review-form', { title: 'Add review' });
 };
